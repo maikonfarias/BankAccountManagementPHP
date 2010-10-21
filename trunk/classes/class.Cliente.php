53cd5f3983b179ce506843d7163ba72e74446eb5
maikonfarias <maikonfarias@0aff0bf0-ec3c-ecff-eea2-318114dde473>
2010-10-21 20:48:16 +0000

    cadastro de clientes

--- trunk/classes/class.Cliente.php
+++ trunk/classes/class.Cliente.php
@@ -1,4 +1,5 @@
 <?php
+include 'classes/class.DB.php';
 
 /**
  * classe para manipular clientes
@@ -193,6 +194,19 @@ class Cliente{
   }
   
   /**
+   * Seta o data de nascimento do cliente
+   * @param $value string data de nascimento do cliente
+   */
+  public function setDataNascimento($value){
+  	$value = (string) $value;
+    if (!empty($value)) {
+      $this->data_nascimento = $value;
+    } else {
+      throw new Exception('data de nascimento deve ser string');
+    }
+  }
+  
+  /**
    * Seta o cpf do cliente
    * @param $value string cpf do cliente
    */
@@ -204,87 +218,141 @@ class Cliente{
       throw new Exception('cpf deve ser string');
     }
   }
+  
+  /**
+   * Seta o rg do cliente
+   * @param $value string rg do cliente
+   */
+  public function setRg($value){
+  	$value = (string) $value;
+    if (!empty($value)) {
+      $this->rg = $value;
+    } else {
+      throw new Exception('rg deve ser string');
+    }
+  }
 
   /**
    * Cadastra o cliente
    * @return true ou false se der erro
    */
-  public function Cadastra() {
-  	$sSQL = "INSERT INTO clientes (id_cliente, nome, logradouro, numero, cep, cpf) 
-			VALUES (".$this->id_cliente.",'".$this->nome."','".$this->logradouro."',".$this->numero.",'".$this->cep."','".$this->cpf."')";
-	$oConn = new DB();
-	$res = mysql_query($sSQL,$oConn->Conn());
-	
-	if ($res) {
-		return true;
-	} else {
-		throw new Exception('ERRO DE SQL: '.mysql_error());
-	}
+  public function cadastra() {
+    $sSQL = "
+      INSERT INTO clientes (id_cliente, nome, logradouro, numero, cep, data_nascimento, cpf, rg) 
+			VALUES (".$this->id_cliente.",'".$this->nome."','".$this->logradouro."',".$this->numero.",'".$this->cep."', '".$this->data_nascimento."','".$this->cpf.", '".$this->rg."')
+    ";
+    $oConn = new DB();
+    $res = $oConn->query($sSQL);
+    
+    if ($res) {
+      return true;
+    } else {
+      throw new Exception('ERRO DE SQL: '.mysql_error());
+    }
   }
   
   /**
    * Carrega o cliente
    * @return true ou false se der erro
    */
-  public function Carrega($value){
-  	$sSQL = "SELECT nome, logradouro, numero, cep, cpf FROM clientes WHERE id_cliente = '".$value."'";
+  public function carrega($value){
+    $sSQL = "SELECT nome, logradouro, numero, cep, data_nascimento, cpf, rg FROM clientes WHERE id_cliente = '".$value."'";
 	
-	$oConn = new DB();
-	$res = mysql_query($sSQL,$oConn->Conn());
-	
-	if ($res) {
-		$oResultado = mysql_fetch_object($res);
-		$this->id_cliente = $value;
-		$this->nome = $oResultado->nome;
-		$this->logradouro = $oResultado->logradouro;
-		$this->numero = $oResultado->numero;
-		$this->cep = $oResultado->cep;
-		$this->cpf = $oRestuldado->cpf;
-		return true;
-	} else {
-		throw new Exception('ERRO DE SQL: '.mysql_error());
-	}
+    $oConn = new DB();
+    $res = $oConn->query($sSQL);
+    
+    if ($res) {
+      $oResultado = mysql_fetch_object($res);
+      
+      $this->id_cliente       = $value;
+      $this->nome             = $oResultado->nome;
+      $this->logradouro       = $oResultado->logradouro;
+      $this->numero           = $oResultado->numero;
+      $this->cep              = $oResultado->cep;
+      $this->data_nascimento  = $oResultado->data_nascimento;
+      $this->cpf              = $oRestuldado->cpf;
+      $this->rg               = $oRestuldado->rg;
+      
+      return true;
+    } else {
+      throw new Exception('ERRO DE SQL: '.mysql_error());
+    }
   }
   
   /**
    * Edita o cliente
    * @return true ou false se der erro
    */
-  public function Editar() {
-  	$sSQL = "UPDATE clientes SET 
-					nome = '".$this->nome."',
-					logradouro = '".$this->logradouro."',
-					numero = ".$this->numero.",
-					cep = '".$this->cep."',
-					cpf = '".$this->cpf."'
-  			  WHERE id_cliente = ".$this->id_cliente;	
-	$oConn = new DB();
-	$res = mysql_query($sSQL,$oConn->Conn());
-	
-	if ($res) {
-		return true;
-	} else {
-		throw new Exception('ERRO DE SQL: '.mysql_error());
-	}
+  public function editar() {
+    $sSQL = "UPDATE clientes SET 
+              nome              = '".$this->nome."',
+              logradouro        = '".$this->logradouro."',
+              numero            = ". $this->numero.",
+              cep               = '".$this->cep."',
+              data_nascimento   = '".$this->data_nascimento."',
+              cpf               = '".$this->cpf."',
+              rg                = '".$this->rg."'
+              WHERE id_cliente  = ". $this->id_cliente;	
+    $oConn = new DB();
+    $res = $oConn->query($sSQL);
+    
+    if ($res) {
+      return true;
+    } else {
+      throw new Exception('ERRO DE SQL: '.mysql_error());
+    }
   }
   
   /**
    * Remove o cliente
    * @return true ou false se der erro
    */
-  public function Remover($value){
-  	$sSQL = " DELETE FROM clientes
-  			  WHERE id_cliente = ".$value;	
-	$oConn = new DB();
-	$res = mysql_query($sSQL,$oConn->Conn());
-	
-	if ($res) {
-		return true;
-	} else {
-		throw new Exception('ERRO DE SQL: '.mysql_error());
-	}
+  public function remover($value){
+    $sSQL = " DELETE FROM clientes
+              WHERE id_cliente = ".$value;	
+    $oConn = new DB();
+    $res = $oConn->query($sSQL);
+    
+    if ($res) {
+      return true;
+    } else {
+      throw new Exception('ERRO DE SQL: '.mysql_error());
+    }
   }
-
   
+  /**
+   * Metodo estatico para listar os clientes em array
+   * @return array clientes
+   */
+  public static function listar ($sWhere = '') {
+    $sSQL = 'SELECT id_cliente, nome, logradouro, numero, cep, data_nascimento, cpf, rg FROM clientes';
+    if (!empty($sWhere)) {
+      $sSQL .= 'WHERE '.$sWhere;
+    }
+    $oConn = new DB();
+    $res = $oConn->query($sSQL);
+    
+    $aClientes = array();
+    
+    while ($oRes = pg_fetch_object($res)) {
+      $cliente = new self();
+      $cliente->setIdCliente(       $oRes->id_cliente);
+      $cliente->setNome(            $oRes->nome);
+      $cliente->setLogradouro(      $oRes->logradouro);
+      $cliente->setNumero(          $oRes->numero);
+      $cliente->setCep(             $oRes->cep);
+      $cliente->setDataNascimento(  $oRes->data_nascimento);
+      $cliente->setCpf(             $oRes->cpf);
+      $cliente->setRg(              $oRes->rg);
+      $aClientes[] = $cliente;
+    }
+    
+    return $aClientes;
+  }
 }
+
+
+
+
+
 ?>
---
+++ trunk/index.php
@@ -0,0 +1,15 @@
+<?php
+include 'classes/class.DB.php';
+try{
+  $conn = new DB();
+} catch(Exception $e) {
+  echo 'ERRO: ' . $e->getMessage();
+  die;
+}
+echo '<title>Conta Bancaria</title>';
+echo 'Conectado!<br><br>';
+
+$res = $conn->query("SELECT '321etset' as teste");
+$oRes = pg_fetch_object($res);
+echo $oRes->teste;
+
